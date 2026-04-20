c3f4dcb9c5d2bdf398da4ffb193a494163598e4d
ALICE BERTHELOT <aliceberthelot.pro@gmail.com>
2026-04-20 23:36:53 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+**Jinji** is a multi-company HRIS application specializing in paid leave management. I am currently developing this project in preparation for my upcoming exam for the professional certification “Application Designer and Developer”.
+
+# Main technologies used :
+
+- Next.js (ReactJS / TypeScript framework)
+- Spring Boot (Java framework)
+- PostgreSQL
---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import {
+  Inclusive_Sans,
+  Nunito_Sans,
+} from "next/font/google";
+import "./globals.css";
+
+const inclusiveSans = Inclusive_Sans({
+  variable: "--font-inclusive-sans",
+  subsets: ["latin"],
+});
+
+const nunitoSans = Nunito_Sans({
+  variable: "--font-nunito-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "jinji",
+  description: "multi-company HRIS application specializing in paid leave management",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inclusiveSans.variable} ${nunitoSans.variable} antialiased`}
+      >
+          <main>{children}</main>
+      </body>
+    </html>
+  );
+}
